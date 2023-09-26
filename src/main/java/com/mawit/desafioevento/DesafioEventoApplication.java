038a0d1e0a37238799523b05f433076fe920e9a2
Marvin <marvinwanzuita@gmail.com>
2023-09-26 13:17:04 -0300

    desafio completo

---
+++ src/main/java/com/mawit/desafioevento/DesafioEventoApplication.java
@@ -0,0 +1,13 @@
+package com.mawit.desafioevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEventoApplication.class, args);
+	}
+
+}
